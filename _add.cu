5eef685f3343eabb6afa98f745feb5c799cc020f
Thomas R Stovall <trstovall@gmail.com>
2022-03-01 20:26:33 -0600

    cpp to c

--- _add.cu
+++ _add.cu
@@ -4,32 +4,22 @@
 
 #define DIM 1000
 
-struct complex {
-  float x, i;
-  complex(float a, float b) : r(a), i(b) {}
-  __device__ float mag2(void) {
-    return r * r + i * i;
-  }
-  __device__ complex operator *(const complex& a) {
-    return complex(r*a.r - i*a.i, i*a.r + r*a.i);
-  }
-  __device__ complex operator +(const complex& a) {
-    return complex(r+a.r, i+a.i);
-  }
-};
-
 __device__ int julia(int x, int y) {
-  const float scale = 1.5;
-  float jx = scale * (float)(DIM/2 - x) / (DIM/2);
-  float jy = scale * (float)(DIM/2 - y) / (DIM/2);
+  float r0 = 0, r = 1.5 * (float)(DIM/2 - x) / (DIM/2);
+  float i0 = 0, i = 1.5 * (float)(DIM/2 - y) / (DIM/2);
+  float m = 0;
   
   complex c(-0.8, 0.156);
   complex a(jx, jy);
   
   for (int i=0; i<200; i++) {
-    a = a * a + c;
-    if (a.mag2() > 1000)
+    r0 = r*r - i*i - 0.8;
+    i0 = i*r + i*r + 0.156;
+    m = r*r + i*i;
+    if (m > 1000)
       return 0;
+    r = r0;
+    i = i0;
   }
   
   return 1;
@@ -47,14 +37,15 @@ __global__ void kernel(unsigned char *p) {
 }
 
 int main(void) {
-  unsigned char *bm = (void*) malloc(DIM*DIM);
+  unsigned char *bm = (unsigned char*) malloc(DIM*DIM);
   unsigned char *bm_dev;
   
   cudaMalloc((void**)&bm_dev, DIM*DIM);
   kernel<<<DIM, DIM, 1>>>(bm_dev);
+  cudaDeviceSynchronize();
   cudaMemcpy(bm, bm_dev, DIM*DIM, cudaMemcpyDeviceToHost);
   
-  img = fopen("julia.pgm", "wb");
+  FILE *img = fopen("julia.pgm", "wb");
   if (img == NULL) {
     perror("ERROR: Cannot open output file");
     exit(EXIT_FAILURE);
@@ -65,7 +56,9 @@ int main(void) {
   fprintf(img, "255\n");
 
   for (int i=0 ;i<DIM*DIM; i++)
-    fputc(bm[i]);
+    fputc(bm[i], img);
+
+  fclose(img);
 
   cudaFree(bm_dev);
 }
