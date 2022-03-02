c7288587c29ca6cc4ac53123e801b8c264e45c2d
Thomas R Stovall <trstovall@gmail.com>
2022-03-02 05:23:29 -0600

    fixed julia c

--- _add.cu
+++ _add.cu
@@ -9,13 +9,11 @@ __device__ int julia(int x, int y) {
   float i0 = 0, i = 1.5 * (float)(DIM/2 - y) / (DIM/2);
   float m = 0;
   
-  complex c(-0.8, 0.156);
-  complex a(jx, jy);
-  
-  for (int i=0; i<200; i++) {
+  for (int n=0; n<200; n++) {
+    // printf("n: %d r0: %.2f i0: %.2f\n", n, r0, i0);
     r0 = r*r - i*i - 0.8;
     i0 = i*r + i*r + 0.156;
-    m = r*r + i*i;
+    m = r0*r0 + i0*i0;
     if (m > 1000)
       return 0;
     r = r0;
@@ -30,21 +28,55 @@ __global__ void kernel(unsigned char *p) {
   int y = blockIdx.y;
   int z = x + y * gridDim.x;
   
-  p[z*4 + 0] = 255 * julia(x,y);
-//  p[z*4 + 1] = 0;
-//  p[z*4 + 2] = 0;
-//  p[z*4 + 3] = 255;
+  if (x < DIM && y < DIM) {
+    p[z] = 255 * julia(x, y);
+    // printf("p: %d z: %d\n", p[z], z);
+  }
 }
 
 int main(void) {
   unsigned char *bm = (unsigned char*) malloc(DIM*DIM);
   unsigned char *bm_dev;
   
-  cudaMalloc((void**)&bm_dev, DIM*DIM);
-  kernel<<<DIM, DIM, 1>>>(bm_dev);
-  cudaDeviceSynchronize();
+  cudaError_t err;
+  err = cudaSetDevice(0);
+  if (err != cudaSuccess) {
+    exit(EXIT_FAILURE);
+  }
+
+  err = cudaMalloc((void**)&bm_dev, DIM*DIM);
+  if (err != cudaSuccess) {
+    perror("ERROR: cudaMalloc");
+    exit(EXIT_FAILURE);
+  }
+
+  cudaStream_t stream;
+  err = cudaStreamCreate(&stream);
+  if (err != cudaSuccess) {
+    perror("ERROR: cudaStreamCreate");
+    exit(EXIT_FAILURE);
+  }
+
+  dim3 grid(DIM, DIM);
+  kernel<<<grid, 1, 0, stream>>>(bm_dev);
+  err = cudaStreamSynchronize(stream);
+  if (err != cudaSuccess) {
+    perror("ERROR: cudaStreamSynchronize");
+    exit(EXIT_FAILURE);
+  }
+
   cudaMemcpy(bm, bm_dev, DIM*DIM, cudaMemcpyDeviceToHost);
-  
+  if (err != cudaSuccess) {
+    perror("ERROR: cudaMemcpy D2H");
+    exit(EXIT_FAILURE);
+  }
+
+  err = cudaStreamDestroy(stream);
+  if (err != cudaSuccess) {
+    perror("ERROR: cudaStreamDestroy");
+    exit(EXIT_FAILURE);
+  }
+
   FILE *img = fopen("julia.pgm", "wb");
   if (img == NULL) {
     perror("ERROR: Cannot open output file");
@@ -55,8 +87,9 @@ int main(void) {
   fprintf(img, "%d %d\n", DIM, DIM);
   fprintf(img, "255\n");
 
-  for (int i=0 ;i<DIM*DIM; i++)
+  for (int i=0; i<DIM*DIM; i++)
     fputc(bm[i], img);
+    // fprintf(img, "%d\n", bm[i]);
 
   fclose(img);
 
